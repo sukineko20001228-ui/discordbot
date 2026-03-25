950ddf840a261449a3f75576532560c475579a83
sukineko20001228-ui <sukineko20001228@gmail.com>
2026-03-25 22:09:56 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,4 +64,18 @@ async def on_message(message):
 
 TOKEN = os.getenv("TOKEN")
 
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is alive"
+
+def run_web():
+    app.run(host="0.0.0.0", port=10000)
+
+threading.Thread(target=run_web).start()
+
 client.run(TOKEN)
